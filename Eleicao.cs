c70d77d27c441b5521d9c799733f1815b852349a
Diego Boschetti <dlboschetti@gmail.com>
2020-08-05 00:53:27 -0300

    first commit

---
+++ Eleicao.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace exercicio_logica_programacao9
+{
+    public class Eleicao
+    {
+        public int totalEleitores = 100000;
+
+        public int votosValidos = 80000;
+
+        public int votosBrancos = 15000;
+
+        public int votosNulos = 5000;
+
+        public double PercVotosValidos()
+        {
+            double percVotos  = (double)votosValidos / (double)totalEleitores;
+            return percVotos;
+        }
+
+        public double PercVotosBrancos()
+        {
+            double percVotos = (double)votosBrancos / (double)totalEleitores;
+            return percVotos;
+        }
+
+        public double PercVotosNulos()
+        {
+            double percVotos = (double)votosNulos / (double)totalEleitores;
+            return percVotos;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace exercicio_logica_programacao9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Percentual de votos em relação ao total de eleitores
+
+            //Considerando as variáveis abaixo:
+
+            //            total de eleitores
+            //              válidos
+            //              votos brancos
+            //              nulos
+
+            //Faça uma classe com 3 métodos que calculam:
+
+            //      -o percentual do votos válidos em relação ao total de eleitores,
+            //      -o percentual de brancos em relação ao total de eleitores
+            //      - o percentual de nulos em relação ao total de eleitores.
+
+            //Dica: “em relação ao total” significa que você deve dividir, por exemplo, “nulos” pelo total de eleitores,
+            //válidos pelo total de eleitores, etc…
+
+            //Utilize programação orientada a objetos.
+
+            Eleicao eleicao = new Eleicao();
+
+            Console.WriteLine("Informar total de eleitores:");
+            eleicao.totalEleitores = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Informar total de votos validos:");
+            eleicao.votosValidos = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Informar total de votos nulos:");
+            eleicao.votosNulos = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Informar total de votos em branco:");
+            eleicao.votosBrancos = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine($"Percentual de votos válidos:  {eleicao.PercVotosValidos()}.");
+            Console.WriteLine($"Percentual de votos nulos:  {eleicao.PercVotosNulos()}.");
+            Console.WriteLine($"Percentual de votos brancos:  {eleicao.PercVotosBrancos()}.");
+
+
+
+
+        }
+    }
+}
